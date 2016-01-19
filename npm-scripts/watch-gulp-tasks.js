775384533551de27693000c60d35d99761d39cd2
Michael Nigh <mic.nigh@gmail.com>
2016-01-18 22:20:46 -0800

    build before starting watch

--- npm-scripts/watch-gulp-tasks.js
+++ npm-scripts/watch-gulp-tasks.js
@@ -1,6 +1,8 @@
 "use strict";
 let spawn = require("cross-spawn");
 
+require("./build-gulp-tasks");
+
 function spawnHelper(command) {
   console.log(command.join(" "));
   spawn(command[0], command.slice(1), { stdio: "inherit" });
