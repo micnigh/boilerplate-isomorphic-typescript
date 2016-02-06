dc290c4af0913b6a659cf61d3487d4a1292d8411
Michael Nigh <mic.nigh@gmail.com>
2016-02-06 10:20:55 -0800

    cleanup - unused task

--- tasks/build/js/index.ts
+++ tasks/build/js/index.ts
@@ -3,12 +3,10 @@ import { GulpTask, GulpBuildTask } from "../../../gulpfile.types";
 import { GulpConfig } from "../../../gulpfile.config.types";
 
 import * as buildJSClientTask from "./client/";
-import * as buildJSServerTask from "./server/";
 
 export let generateTask = (gulp: Gulp, config: GulpConfig): GulpBuildTask => {
   let gulpTask = new GulpBuildTask();
   gulpTask.addChildTask(buildJSClientTask.generateTask(gulp, config));
-  gulpTask.addChildTask(buildJSServerTask.generateTask(gulp, config));
 
   gulp.task("build:js", gulpTask.childBuildTasks);
   gulp.task("watch:js", gulpTask.childWatchTasks);
