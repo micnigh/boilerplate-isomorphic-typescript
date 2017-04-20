93e810b566cea562c6beb8f3263520f8628e41c0
Michael Nigh <mic.nigh@gmail.com>
2017-04-20 10:41:23 -0700

    Update deps, simplify tsconfig.json files

--- npm-scripts/postinstall.ts
+++ npm-scripts/postinstall.ts
@@ -3,12 +3,7 @@ import * as fs from "fs";
 import { dllLibPath, dllLibFileName } from "../config";
 
 export let runTasks = async () => {
-
-  // copy sample files
-  try { fs.statSync("tsconfig.json"); } catch (e) { fs.writeFileSync("tsconfig.json", fs.readFileSync("tsconfig.sample.json")); }
-  try { fs.statSync("server/tsconfig.json"); } catch (e) { fs.writeFileSync("server/tsconfig.json", fs.readFileSync("server/tsconfig.sample.json")); }
-
-  // build dll lib if needed
+  // build dll lib
   await taskLib.runTask();
 };
 
